85d8a3bea3ce5764c5dff08c2c68664342e28071
agendreau <alex.gendreau24@gmail.com>
2015-09-15 22:12:41 -0600

    loads images and text

--- Examples/HelloServer.c
+++ Examples/HelloServer.c
@@ -14,23 +14,27 @@ char * contentType(char filename[]){
     char *token;
     char delim[2] = ".";
     if(strcmp(filename,"/")==0){
-        return "Content-Type: text/html\r\n\r\n";
+        return "Content-Type: text/html\r\n";
     }
     token = strtok(filename, delim);
     token = strtok(NULL, delim); //get what comes last. hack might have to fix
     if(strcmp(token,"html")==0 || strcmp(token,"htm")==0){
-        content = "Content-Type: text/html\r\n\r\n";
+        content = "Content-Type: text/html\r\n";
     }
     else if(strcmp(token,"txt")==0) {
-        content = "Content-Type: text/plain\r\n\r\n";
+        content = "Content-Type: text/plain\r\n";
     }
     
     else if(strcmp(token,"gif")==0){
-        content = "Content-Type: image/gif\r\n\r\n";
+        content = "Content-Type: image/gif\r\n";
+    }
+    
+    else if(strcmp(token,"png")==0){
+        content = "Content-Type: image/png\r\n";
     }
     
     else if(strcmp(token,"jpg")==0){
-        content = "Content-Type: image/jpg\r\n\r\n";
+        content = "Content-Type: image/jpg\r\n";
     }
     
     else {
@@ -41,11 +45,11 @@ char * contentType(char filename[]){
     
 }
 
-long int GetFileSize(char* filename)
+long GetFileSize(char* filename)
 { /*Got from 
    http://cboard.cprogramming.com/c-programming/79016-function-returns-number-bytes-file.html
    */
-    long int size;
+    long size;
     FILE *f;
     
     f = fopen(filename, "rb");
@@ -63,7 +67,7 @@ void readLine(char firstLine[], int sock) {
     char c;
     int len;
     len = recv(sock, &c, 1, 0);
-    printf("%c\n",c);
+    //printf("%c\n",c);
     while (len == 1 && c != '\n') {
         firstLine[i] = c;;
         i++;
@@ -74,84 +78,67 @@ void readLine(char firstLine[], int sock) {
     //printf("%s",firstLine);
 }
 
-bool readRequest(char firstLine[], char host[], int sock) {
-    bool keep_alive = false;
+void readRequest(char firstLine[], char host[], char connection[],int sock) {
     char buf[1024];
+    char copy_buffer[1024];
     int len;
-    readLine(firstLine, sock);
-    printf("first line read: %s\n", firstLine);
-    //readLine(host,sock);
-    //printf("%s",host);
-    //read remaining bytes
-    //readLine(buf,sock);
-    /*while(1) {
-        if(strlen(buf)>=10){
-            char header[100];
-            strncpy(header,buf,10);
-            header[10]='\0';
-            printf("header: %s\n",header);
-            if(strcmp(header,"User-Agent")==0){
-                break;
-            }
-            if(strcmp(header,"Connection")==0) {
-                printf("got first comparison\n");
-                char actual_header[100];
-                strncpy(actual_header,buf,22);
-                actual_header[22]='\0';
-                if(strcmp(actual_header,"Connection: keep-alive")==0){
-                    printf("connection found keep alive\n");
-                    keep_alive = true;
-                }
-            }
+    char delim[2]=" ";
+    readLine(buf, sock);
+    strcpy(firstLine,buf);
+    //printf("first line read: %s\n", firstLine);
+  
+    
+    printf("the buffer is: %s\n",buf);
+    while(strlen(buf)>2) {
+        char *token;
+        strcpy(copy_buffer,buf);
+        token = strtok(copy_buffer, delim);
+        printf("token: %s\n",token);
+        if(strcmp(token,"host:")==0 || strcmp(token,"HOST:")==0){
+            strcpy(host,buf);
         }
-        memset(buf,0,strlen(buf));
+        else if(strcmp(token,"Connection:")==0){
+            strcpy(connection,buf);
+        }
         readLine(buf,sock);
+        printf("the new buffer is: %s\n",buf);
+        /*memset(buf,0,strlen(buf));
         printf("new buffer\n");
         printf("%s\n",buf);
-        printf("cmp value: %d\n",strcmp(buf,"\n"));
-    }*/
-    return keep_alive;
+        printf("cmp value: %d\n",strcmp(buf,"\n"));*/
+    }
+    
 }
 
 void sendBinary(int sock,char * filename){
     char buf[1024];
     FILE *fp;
 
-    long int filesize = GetFileSize(filename);
-    //long filesize = (long)(lseek(fp,(off_t)0,SEEK_END));
-    //(void)lseek(fp,(off_t)0,SEEK_SET);
-    //char contentHeader[100];
-    //char * contentHeader = "Content-Length: 3536283\r\n";
-    //sprintf(contentHeader,"Content-Length: %zu\r\n\r\n",filesize);
-    //printf("content length: %s\n",contentHeader);
+    long filesize = GetFileSize(filename);
+
+    char contentHeader[100];
+    sprintf(contentHeader,"Content-Length: %ld\r\n\r\n",filesize);
+    send(sock,contentHeader,strlen(contentHeader),0);
     
-    //int x = send(sock,contentHeader,strlen(contentHeader),0);
+    printf("File size: %s\n",contentHeader);
+
+    /*char contentAlive[100];
+    sprintf(contentAlive,"Connection: keep alive\r\n\r\n");
+    send(sock,contentAlive,strlen(contentAlive),0);*/
     
-    char contentAlive[100];
-    //sprintf(contentAlive,"Connection: keep alive\r\n");
-    //send(sock,contentAlive,strlen(contentAlive),0);
-    //printf("content size: %d\n",x);
+    
     size_t total = 0;
-    //char new_buf[filesize];
-    //bytesRead = fread(new_buf,sizeof(char),filesize,fp);
-    //sendmsg(sock,new_buf,0);
     int success;
     fp = fopen(filename,"rb"); //add error checking
     size_t bytesRead;
     while (( bytesRead = fread( buf, sizeof(char), 1024, fp )) > 0 ) {
         total+=bytesRead;
-        //printf("this is the buffer: %s\n",buf);
         success = send(sock, buf, bytesRead,0);
-        //printf("did we send: %d\n",success);
-        //printf("did we send: %zu\n",bytesRead);
-        //memset(buf,0,strlen(buf));
-        
     }
-    sleep(1);
-    //printf("size of char: %lu",sizeof(char));
+    
+    //sleep(1);
     printf("bytes read: %ld\n",total);
     fclose(fp);
-    //close(sock);
 }
 
 
@@ -160,20 +147,21 @@ void *processRequest(void *s) { //,char *document_root) {
     int sock = *((int *) s);
     char firstLine[1024];
     char host[1024];
-    char line[1024];
+    char connection[1024];
     char delim[2] = " ";
     char crlf[3] = "\r\n";
-    char filename[100];
+    char uri[100];
+    char filename[1024];
     char *status1 = "HTTP/1.0 200 OK\r\n";
     //char *content1 = "Content-Type: text/html\r\n\r\n";
     int num = 0;
     char *token;
-    char *default_file = "/index.html";
-    char document_root[1024];
-    //document_root = "/Users/Alex/Desktop/Fall2015/Networks/www";
+    char *default_file = "index.html";
+    //char document_root[1024];
+    char * document_root = "/Users/Alex/Downloads";
    
     
-    bool keep_alive = readRequest(firstLine,host,sock);
+    readRequest(firstLine,host,connection,sock);
     printf("finished reading request\n");
     //readFirstLine(firstLine, sock);
     printf("%s\n", firstLine);
@@ -182,17 +170,38 @@ void *processRequest(void *s) { //,char *document_root) {
     //token = "index.html";
     
     //strcpy(filename, ".");
-    strcat(filename, token);
+    strcat(uri, token);
     
-    printf("%s\n", filename);
+    printf("Suffix: %s\n", uri);
+    
+    if(strcmp(uri,"/")==0){
+        strcat(uri,default_file);
+    }
+    
     char forSuffix[100];
-    strcpy(forSuffix,filename);
+    strcpy(forSuffix,uri);
     char *content = contentType(forSuffix);
     
-    FILE *fp;
-    printf("keep alive: %d\n",keep_alive);
-    printf("%s\n",filename);
-    if(strcmp(filename,"/")==0){
+    strcpy(filename,document_root);
+    strcat(filename,uri);
+    
+    printf("Full Path: %s\n",filename);
+    printf("content type: %s\n",content);
+    send(sock,status1,strlen(status1),0);
+    send(sock,content,strlen(content),0);
+    
+    printf("This is the host: %s\n",host);
+    printf("This is the connection: %s\n",connection);
+    
+    send(sock,connection,strlen(connection),0);
+    
+    sendBinary(sock,filename);
+    
+    
+    //add waiting here
+    close(sock);
+
+    /*if(strcmp(filename,"/")==0){
         printf("here1\n");
         char * index_file = "/Users/Alex/Downloads/index.html";
         //long imageSize = GetFileSize(index_file);
@@ -209,23 +218,7 @@ void *processRequest(void *s) { //,char *document_root) {
         printf("content: %s\n",content);
         send(sock, content, strlen(content), 0);
         sendBinary(sock,index_file);
-        /*while (fgets(line, 1024, fp) != 0) {
-         send(sock, line, strlen(line), 0);
-         }*/
-        /*char filepath[1024];
-        //strcpy(filepath,document_root);
-        //strcat(filepath,"/index.html");
-        fp = fopen("/Users/Alex/Desktop/Fall2015/Networks/www/index.html","r");
-        if (fp == NULL) printf("Error\n");
-        send(sock, status1, strlen(status1), 0);
-        //send(sock, content1, strlen(content1), 0);
-        printf("content: %s\n",content);
-        send(sock, content, strlen(content), 0);
-        while (fgets(line, 1024, fp) != 0) {
-            send(sock, line, strlen(line), 0);
-        }
-        fclose(fp);*/
-        
+ 
 
     }
     else {
@@ -245,15 +238,15 @@ void *processRequest(void *s) { //,char *document_root) {
         printf("content: %s\n",content);
         send(sock, content, strlen(content), 0);
         sendBinary(sock,filename_png);
-        /*while (fgets(line, 1024, fp) != 0) {
+        while (fgets(line, 1024, fp) != 0) {
             send(sock, line, strlen(line), 0);
-        }*/
+        }
 
     }
         //here is where i should wait.
     
     
-    close(sock);
+    close(sock);*/
     return NULL;
 }
 
