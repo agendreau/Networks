d24b7f29805e684b253d98a0df7b2ee24dbcb051
agendreau <alex.gendreau24@gmail.com>
2015-12-09 09:37:29 -0700

    comments and removed prints

--- PA4/simpler_proxy.c
+++ PA4/simpler_proxy.c
@@ -22,11 +22,17 @@
 
 pthread_mutex_t lock;
 
-/* Processes the client request
- * Reads the request
- * Checks for errors in request and sends back the correct error message
- * Closes the connection immediately after message is passed through the server and back to the client
- * Parses the request, host, uri, and port
+/* 
+ * Processes the client request through the proxy to the server and back again
+ * Reads the request 
+ * Extracts ip/port from client, extracts ip/port from desitnation 
+ * Creates socket to connect proxy to server
+ * Binds this socket to get a port number for the proxy side of the connection
+ * Adds SNAT entry from proxy to server
+ * Connect socket between proxy and server
+ * Relays information between client and server using select statement until session is ended
+ * Deletes SNAT entry and logs information in the proxy.log file
+ * Closes both socket connections
  */
 
 void *processRequest(void *s) { //,char *document_root) {
@@ -35,10 +41,11 @@ void *processRequest(void *s) { //,char *document_root) {
      * http://ubuntuforums.org/showthread.php?t=634216
      */
     
-    //Setting everything up.  Not the best way to manage my memory
+    
     int client_sock = *((int *) s);
     free(s);
     
+    // Get the information about address and port numbers from sockets
     struct sockaddr_in dest_addr;
     socklen_t dest_len = sizeof(dest_addr);
     memset(&dest_addr, 0, dest_len);
@@ -65,24 +72,26 @@ void *processRequest(void *s) { //,char *document_root) {
     
     int server_sock = socket(AF_INET,SOCK_STREAM,0);
     
+    //bind to get a port number for the proxy-server connection on the proxy side
     if (bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
         perror("bind");
-        exit(1);
+        exit(-1);
     }
     
     getsockname(server_sock,(struct sockaddr *)&proxy_addr,&proxy_len);
     
+    //add iptable rule
     pthread_mutex_lock(&lock);
     char comm[1000];
     snprintf(comm, sizeof(comm), "iptables -t nat -A POSTROUTING -p tcp -j SNAT --sport %hu --to-source %s", ntohs(proxy_addr.sin_port),inet_ntoa(client_addr.sin_addr));
     system(comm);
     pthread_mutex_unlock(&lock);
     
-    printf( "Server Destination: %s:%hu\n", inet_ntoa(proxy_addr.sin_addr), ntohs(proxy_addr.sin_port));
+    //printf( "Server Destination: %s:%hu\n", inet_ntoa(proxy_addr.sin_addr), ntohs(proxy_addr.sin_port));
     
-    printf( "Client Source: %s:%hu\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    //printf( "Client Source: %s:%hu\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     
-    printf( "Client Destination: %s:%hu\n", inet_ntoa(dest_addr.sin_addr), ntohs(dest_addr.sin_port));
+    //printf( "Client Destination: %s:%hu\n", inet_ntoa(dest_addr.sin_addr), ntohs(dest_addr.sin_port));
     
     short orig_port = ntohs(dest_addr.sin_port);
     char orig_addr[64];
@@ -94,51 +103,46 @@ void *processRequest(void *s) { //,char *document_root) {
     lt = time(NULL);
     ptr = localtime(&lt);
 
-    strftime(str, 100, "%D %H:%M:%S", ptr);
+    strftime(str, 100, "%D %H:%M:%S", ptr); //get date and time
    
-    if(connect(server_sock,(struct sockaddr *)&dest_addr,dest_len)<0)
-	perror("connect");
-     
+    if(connect(server_sock,(struct sockaddr *)&dest_addr,dest_len)<0){
+        perror("connect");
+        exit(-1);
+    }
+    
     long total_bytes_sent=0;
     long total_bytes_received=0;
 
     
+    //set up socket set for communication
     int sel_ret;
-    //int select_server;
     
     struct timeval tv;
     fd_set socket_set;
-    //fd_set set_server;
     
     FD_ZERO(&socket_set);
-    //FD_ZERO(&set_server);
     char buf[1024];
     memset(buf, '\0', 1024);
     long bytes_client=0;
     long bytes_server=0;
     
     
-    while(1){
+    while(1){//communicate link
         FD_ZERO(&socket_set);
         FD_SET(client_sock,&socket_set);
         FD_SET(server_sock,&socket_set);
         tv.tv_sec = 10;
         tv.tv_usec = 0;
 
-
         sel_ret = select(MAX(client_sock,server_sock)+1,&socket_set,NULL,NULL,&tv);
 
-        //printf("fdset: %d\n",selRet);
-        //selRet=2; //for testing error 500
-        
-        
         if(sel_ret>=1){
             if(FD_ISSET(client_sock,&socket_set)){
                 bytes_client = recv(client_sock,buf,1024,0);
                 send(server_sock,buf,bytes_client,0);
                 total_bytes_sent+=bytes_client;
-		if(bytes_client<=0){
-			printf("read 0 client\n");
+		if(bytes_client<=0){ //session over
+            //printf("read 0 client\n");
 			break;
 		}
 			
@@ -147,8 +151,8 @@ void *processRequest(void *s) { //,char *document_root) {
                 bytes_server = recv(server_sock,buf,1024,0);
                 send(client_sock,buf,bytes_server,0);
                 total_bytes_received+=bytes_server;
-		if(bytes_server<=0){
-			printf("read 0 client\n");
+		if(bytes_server<=0){ //session over
+			//printf("read 0 client\n");
 			break;
 		}
             }
@@ -161,11 +165,12 @@ void *processRequest(void *s) { //,char *document_root) {
      //clean up
     close(client_sock);
     close(server_sock);
-    printf("at cleanup\n");
+    //printf("at cleanup\n");
     pthread_mutex_lock(&lock);
     snprintf(comm, sizeof(comm), "iptables -t nat -D POSTROUTING -p tcp -j SNAT --sport %hu --to-source %s", ntohs(proxy_addr.sin_port),inet_ntoa(client_addr.sin_addr));
     system(comm);
     
+    //append onto log file
     FILE *fp = fopen("proxy.log","a");
 
     char log[2048];
@@ -210,13 +215,13 @@ void run_server(int port)
     
     if((bind(sock,(struct sockaddr *)&server,len)) == -1)
     {
-        printf("bind\n");
+        perror("bind");
         exit(-1);
     }
     
     if((listen(sock,5))==-1)
     {
-        printf("listen\n");
+        perror("listen");
         exit(-1);
     }
     //int i = 0;
@@ -224,7 +229,7 @@ void run_server(int port)
     {
         if((cli = accept(sock, (struct sockaddr *)&client,&len))==-1)
         {
-            printf("accept\n");
+            perror("accept");
             exit(-1);
         }
         //i++;
@@ -246,16 +251,16 @@ int main(int argc, char *argv[]){
         exit(0);
     }
     else {
-        //Add DNAT rule
-	int port = atoi(argv[1]);
+        //Clear nat tables and then add DNAT
+        int port = atoi(argv[1]);
+        char clear = "iptables -t nat -F";
         char comm[1000];
         snprintf(comm, sizeof(comm), "iptables -t nat -A PREROUTING -p tcp -i eth0 -j DNAT --to 192.168.0.1:%d", port);
+        system(clear); //clear any prerouting rules, we only want ours
         system(comm);
         //FILE *fp = fopen("proxy.log","w"); //create a new one for each time running server
         //fclose(fp);
         run_server(port);
-        snprintf(comm, sizeof(comm), "iptables -t nat -D PREROUTING -p tcp -i eth0 -j DNAT --to 192.168.0.1:%d", port);
-        system(comm);
     }
     return 0;
 }
